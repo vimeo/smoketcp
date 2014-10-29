c057214f30838566c29c4e2ff2ef417b589bc8dc
Joshua C. Forest <jforest@cashstar.com>
2014-10-29 13:43:56 -0400

    fix formatting again

--- smoketcp.go
+++ smoketcp.go
@@ -28,9 +28,9 @@ func doEvery(d time.Duration, f func(*statsd.Client, string, bool), s *statsd.Cl
 func process_targets(s *statsd.Client, target_file string, debug bool) {
 	content, err := ioutil.ReadFile(target_file)
 	if err != nil {
-    if debug {
-		  fmt.Println("couldn't open targets file: %s", target_file)
-    }
+		if debug {
+			fmt.Println("couldn't open targets file: %s", target_file)
+		}
 		return
 	}
 	targets := strings.Split(string(content), "\n")
@@ -51,28 +51,28 @@ func test(target string, s *statsd.Client, debug bool) {
 	pre := time.Now()
 	conn, err := net.Dial("tcp", target)
 	if err != nil {
-    if debug {
-		  fmt.Println("connect error", target)
+		if debug {
+			fmt.Println("connect error", target)
 		}
-    s.Inc(fmt.Sprintf("%s.%s.dial_failed", subhost, port), 1, 1)
+		s.Inc(fmt.Sprintf("%s.%s.dial_failed", subhost, port), 1, 1)
 		return
 	}
 	duration := time.Since(pre)
 	ms := int64(duration / time.Millisecond)
-  if debug {
-	  fmt.Printf("%s.%s.duration %d\n", subhost, port, ms)
-  }
+	if debug {
+		fmt.Printf("%s.%s.duration %d\n", subhost, port, ms)
+	}
 	s.Timing(fmt.Sprintf("%s.%s", subhost, port), ms, 1)
 	conn.Close()
 }
 
 func main() {
-  var statsd_host = flag.String("statsd_host", "localhost", "Statsd Hostname")
-  var statsd_port = flag.String("statsd_port", "8125", "Statsd port")
-  var bucket = flag.String("bucket", "smoketcp", "Graphite bucket prefix")
-  var target_file = flag.String("target_file", "targets", "File containing the list of targets, ex: server1:80")
-  var debug = flag.Bool("debug", false, "if true, turn on debugging output")
-  flag.Parse()
+	var statsd_host = flag.String("statsd_host", "localhost", "Statsd Hostname")
+	var statsd_port = flag.String("statsd_port", "8125", "Statsd port")
+	var bucket = flag.String("bucket", "smoketcp", "Graphite bucket prefix")
+	var target_file = flag.String("target_file", "targets", "File containing the list of targets, ex: server1:80")
+	var debug = flag.Bool("debug", false, "if true, turn on debugging output")
+	flag.Parse()
 
 	s, err := statsd.Dial(fmt.Sprintf("%s:%s", *statsd_host, *statsd_port), fmt.Sprintf("%s", *bucket))
 	dieIfError(err)
