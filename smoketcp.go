7b4559db955c53023b11f7075842ae7444c55661
Joshua C. Forest <jforest@cashstar.com>
2014-10-29 13:30:32 -0400

    add debug flag, to only output to stdout if set to true

--- smoketcp.go
+++ smoketcp.go
@@ -18,17 +18,19 @@ func dieIfError(err error) {
 	}
 }
 
-func doEvery(d time.Duration, f func(*statsd.Client), s *statsd.Client) {
-	f(s)
+func doEvery(d time.Duration, f func(*statsd.Client, string, bool), s *statsd.Client, target_file string, debug bool) {
+	f(s, target_file, debug)
 	for _ = range time.Tick(d) {
-		f(s)
+		f(s, target_file, debug)
 	}
 }
 
-func process_targets(s *statsd.Client) {
-	content, err := ioutil.ReadFile("targets")
+func process_targets(s *statsd.Client, target_file string, debug bool) {
+	content, err := ioutil.ReadFile(target_file)
 	if err != nil {
-		fmt.Println("couldn't open targets file")
+    if debug {
+		  fmt.Println("couldn't open targets file: %s", target_file)
+    }
 		return
 	}
 	targets := strings.Split(string(content), "\n")
@@ -36,11 +38,11 @@ func process_targets(s *statsd.Client) {
 		if len(target) < 1 {
 			continue
 		}
-		go test(target, s)
+		go test(target, s, debug)
 	}
 }
 
-func test(target string, s *statsd.Client) {
+func test(target string, s *statsd.Client, debug bool) {
 	tuple := strings.Split(target, ":")
 	host := tuple[0]
 	port := tuple[1]
@@ -49,13 +51,17 @@ func test(target string, s *statsd.Client) {
 	pre := time.Now()
 	conn, err := net.Dial("tcp", target)
 	if err != nil {
-		fmt.Println("connect error", target)
-		s.Inc(fmt.Sprintf("%s.%s.dial_failed", subhost, port), 1, 1)
+    if debug {
+		  fmt.Println("connect error", target)
+		}
+    s.Inc(fmt.Sprintf("%s.%s.dial_failed", subhost, port), 1, 1)
 		return
 	}
 	duration := time.Since(pre)
 	ms := int64(duration / time.Millisecond)
-	fmt.Printf("%s.%s.duration %d\n", subhost, port, ms)
+  if debug {
+	  fmt.Printf("%s.%s.duration %d\n", subhost, port, ms)
+  }
 	s.Timing(fmt.Sprintf("%s.%s", subhost, port), ms, 1)
 	conn.Close()
 }
@@ -64,10 +70,12 @@ func main() {
   var statsd_host = flag.String("statsd_host", "localhost", "Statsd Hostname")
   var statsd_port = flag.String("statsd_port", "8125", "Statsd port")
   var bucket = flag.String("bucket", "smoketcp", "Graphite bucket prefix")
+  var target_file = flag.String("target_file", "targets", "File containing the list of targets, ex: server1:80")
+  var debug = flag.Bool("debug", false, "if true, turn on debugging output")
   flag.Parse()
 
 	s, err := statsd.Dial(fmt.Sprintf("%s:%s", *statsd_host, *statsd_port), fmt.Sprintf("%s", *bucket))
 	dieIfError(err)
 	defer s.Close()
-	doEvery(time.Second, process_targets, s)
+	doEvery(time.Second, process_targets, s, *target_file, *debug)
 }
